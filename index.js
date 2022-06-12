7ac8304cbeaf76ab6b366aabce5cbfc9a963f177
mbolonzi-design <mbolonzi.design@gmail.com>
2022-06-12 14:00:01 +0300

    commit tree b98dda27b5de3c86dabaa2d051659cae4274187d

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(driver => driver.toUpperCase() === name.toUpperCase())
+  }
+  
+  function fuzzyMatch(drivers, name) {
+    return drivers.filter(driver => driver.substring(0,1) === name.substring(0,1))
+  }
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name)
+  }
